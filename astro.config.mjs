c6f15cebfb7d923e60600d57f2f398f481db53a5
RodrigoTomeES <rotome@unirioja.es>
2024-03-09 16:00:24 +0100

    feat: enabled inline css, compress files and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from 'astro/config';
 import { i18n } from './src/i18n/config';
+import sitemap from '@astrojs/sitemap';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://cv.rodrigotome.es',
     trailingSlash: 'never',
-    build: { format: 'file' },
+    compressHTML: true,
+    output: 'static',
+    build: { 
+        format: 'file',
+        inlineStylesheets: 'always'
+    },
+    integrations: [
+        sitemap(),
+        compress({ img: false }),
+    ],
     i18n
 });
