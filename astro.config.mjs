941a6894831d2885564bdf6562399501fe37006f
RodrigoTomeES <rotome@unirioja.es>
2024-03-21 18:38:19 +0100

    perf: manglify CSS classes and vars

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { i18n } from './src/i18n/config';
 import sitemap from '@astrojs/sitemap';
 import compress from 'astro-compress';
 import tailwind from '@astrojs/tailwind';
+import rename from 'astro-rename';
+import critters from 'astro-critters';
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,9 +17,14 @@ export default defineConfig({
         inlineStylesheets: 'always'
     },
     integrations: [
-        sitemap(),
+        tailwind({
+          config: {
+            applyBaseStyles: false,
+          },
+        }),
+        rename({ targetExt: ["html"] }),
         compress({ img: false, SVG: false }),
-        tailwind()
+        sitemap(),
     ],
     i18n
 });
